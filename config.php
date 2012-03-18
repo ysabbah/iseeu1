79543b2ae0a53bebd00c6bd794ad2f16f291cb10
Yousef Sabbah <y_sabbah@yahoo.com>
2012-03-18 23:47:23 +0200

    second commit

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $host="localhost";
-$dbUser="root";
-$dbPass="root";
-$db="mydatabase";
+$dbUser="depyspyq2mf";
+$dbPass="YwX7MA8NUUrQ";
+$db="depyspyq2mf";
 mysql_connect($host,$dbUser,$dbPass);
 mysql_select_db($db) or die (mysql_error());
 ?>
